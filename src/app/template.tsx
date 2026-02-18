cef1b076fa8281dc23b52233246e5623f12e4dd2
All Might <ggboykxz@gmail.com>
2026-02-18 19:42:10 +0000

    ajout de Fluidité = Perception de rapidité + Absence de blocages + Anima

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,3 +1,3 @@
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <div className="fade-in">{children}</div>;
+  return <div className="fade-in will-change-transform">{children}</div>;
 }
