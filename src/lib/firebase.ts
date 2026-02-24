ec2f898baa2a715ffe5fbd47b8cceb2f07a3df1e
All Might <ggboykxz@gmail.com>
2026-02-24 16:20:06 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrvk6VF3uPdyn43PI-eXjeQQufh7swTwY",
-  authDomain: "studio-6915614597-eca66.firebaseapp.com",
-  databaseURL: "https://studio-6915614597-eca66-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "studio-6915614597-eca66",
-  storageBucket: "studio-6915614597-eca66.firebasestorage.app",
-  messagingSenderId: "656691616636",
-  appId: "1:656691616636:web:b7cdfb56d387c6666262f9",
-  measurementId: "G-T08MQ98DHE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
